ea60836853632b84bf16948a4ff781b3080ac3d9
cbstodd <cbstodd@gmail.com>
2017-02-21 23:02:57 -0500

    setup app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
-  selector: 'i-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'i-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+    apiRoot:string = 'https://itunes.apple.com/search';
+    results:Object[];
+    loading:boolean;
+
+    constructor(private http:Http) {
+        this.results = [];
+        this.loading = false;
+    }
+
+    search(term:string) {
+
+    }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SearchService } from './search.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [SearchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
