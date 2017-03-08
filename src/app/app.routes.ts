a3306f9f2d786bb24e209b2ccc6676dc45eb1036
cbstodd <cbstodd@gmail.com>
2017-03-07 23:53:58 -0500

    removed hash in favor of path strategy routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -29,4 +29,4 @@ const routes:Routes = [
     { path: '**', component: HomeComponent }
 ];
 
-export const routing:ModuleWithProviders = RouterModule.forRoot(routes, { useHash: true });
+export const routing:ModuleWithProviders = RouterModule.forRoot(routes);
